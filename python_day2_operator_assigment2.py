70c3d8856965323805607c57888bd97147c5df31
Devesh1708 <deveshsreekumar@gmail.com>
2024-08-25 16:50:05 +0530

    Add files via upload

---
+++ python_day2_operator_assigment2.py
@@ -0,0 +1,9 @@
+#Declare two variables and print that which variable is largest using ternary operators
+
+a=10
+b=20
+result="variable a is largest" if a>b else "variable b is largest"
+print(result)
+
+#variable b is largest
+
