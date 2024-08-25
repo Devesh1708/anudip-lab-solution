793dcac5e8b29441f91d3e73f1b099f2e82aaeea
Devesh1708 <deveshsreekumar@gmail.com>
2024-08-25 16:50:30 +0530

    Add files via upload

---
+++ python_day2_operator_assigment3.py
@@ -0,0 +1,7 @@
+#3. Python program to convert the temperature in degree centigrade to Fahrenheit
+
+celsius=30
+fahrenheit = (celsius * 9/5) + 32
+print(celsius,"Celsuis is equal to",fahrenheit,"Fahrenheit")
+
+#30 Celsuis is equal to 86.0 Fahrenheit
