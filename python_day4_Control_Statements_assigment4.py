830625f799972662aa7878c5225e70acca32aca1
Devesh1708 <deveshsreekumar@gmail.com>
2024-08-27 13:17:05 +0530

    Add files via upload

---
+++ python_day4_Control_Statements_assigment4.py
@@ -0,0 +1,17 @@
+#4.Create a Python program that checks if a user-given number is positive, negative, or zero.
+# Take a number as input from the user
+number = float(input("Enter a number: "))
+
+# Check if the number is positive, negative, or zero
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+#OUTPUT
+"""Enter a number: -1
+The number is negative."""
+
+    
