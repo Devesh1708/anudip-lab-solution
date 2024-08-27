828cca9b94943f6cbf025489d09e742a219dd258
Devesh1708 <deveshsreekumar@gmail.com>
2024-08-27 13:16:47 +0530

    Add files via upload

---
+++ python_day4_Control_Statements_assigment2.py
@@ -0,0 +1,13 @@
+#2.Create a Python program that checks whether a person is eligible to vote (18 years or older) based on their age.
+# Take the person's age as input
+age = int(input("Enter your age: "))
+
+# Check if the person is 18 years old or older using if condition
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote yet.")
+
+#OUTPUT
+"""Enter your age: 18
+You are eligible to vote."""
