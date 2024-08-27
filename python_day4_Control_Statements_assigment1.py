0ff6f1052bfb2cb2903dfd743b693c89d003652a
Devesh1708 <deveshsreekumar@gmail.com>
2024-08-27 13:16:40 +0530

    Add files via upload

---
+++ python_day4_Control_Statements_assigment1.py
@@ -0,0 +1,13 @@
+#1.Write a Python program that takes a number as input and prints "Even" if the number is even and "Odd" if it's odd.
+# Take a number as input from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+#OUTPUT
+"""Enter a number: 13
+Odd"""
