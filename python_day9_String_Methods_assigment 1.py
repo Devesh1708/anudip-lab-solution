5d933cf3044983d6580eb7b7373b394843f62db7
Devesh1708 <deveshsreekumar@gmail.com>
2024-09-03 19:18:26 +0530

    Add files via upload

---
+++ python_day9_String_Methods_assigment 1.py
@@ -0,0 +1,27 @@
+#1. Write a Python program to Count all letters, digits, and special symbols from the given string
+#Input = “P@#yn26at^&i5ve” Output: Chars = 8 Digits = 2 Symbol = 3
+
+def count_chars_digits_symbols(input_string):
+    chars = digits = symbols = 0
+
+    for char in input_string:
+        if char.isalpha():
+            chars += 1
+        elif char.isdigit():
+            digits += 1
+        else:
+            symbols += 1
+
+    return chars, digits, symbols
+
+input_string = "P@#yn26at^&i5ve"
+chars, digits, symbols = count_chars_digits_symbols(input_string)
+
+print("Chars = ",chars)
+print("Digits = ",digits)
+print("Symbols = ",symbols)
+
+#OUTPUT
+"""Chars =  8
+Digits =  3
+Symbols =  4 """
